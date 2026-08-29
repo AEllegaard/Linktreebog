16caa65d33b3fe314b134d96e136688200f5491f
Anna Ellegaard Skytte <annaeskytte@gmail.com>
2026-08-29 15:23:22 +0200

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="../public/logoSVG.svg" width="125" height="125" />
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
