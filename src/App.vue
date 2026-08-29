215a4888c06b7b5bb306a35c0b09c2af9f3f02a7
Anna Ellegaard Skytte <annaeskytte@gmail.com>
2026-08-29 16:29:01 +0200

    prøver igen

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <script setup>
 import TheWelcome from './components/TheWelcome.vue'
-import logo from '../public/logoSVG.svg'
-
 </script>
 
 <template>
   <header>
-    <img :src="logo" />
+    <img src="/logoSVG.svg" />
   </header>
 
   <main>
     <TheWelcome />
   </main>
 </template>
+
 
 <style scoped>
 
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,6 +12,13 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
   <div class="titles">
     <h1 class="title">Velkommen til!</h1>
   </div>
+  <div class="buttonsection">
+    <div class="inner">
+      <button>Instagram</button>
+      <button>Instagram</button>
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -26,6 +33,34 @@ width: 100%;
 display: flex;
 justify-content: center;
 margin-top: 2rem;
+}
+
+.buttonsection {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.inner {
+width: 20%;
+display: flex;
+justify-content: center;
+flex-direction: column;
+margin: 1rem;
+}
+
+button {
+  background-color: #D12427;
+  color: #fff;
+  border: none;
+  border-radius: 10px;
+  padding: 1rem;
+  margin: 0.5rem;
+  cursor: pointer;
+  font-size: 1rem;
+  font-family: pt sans;
+  font-weight: 700;
+  margin-bottom: 1rem;
 }
 
 </style>
